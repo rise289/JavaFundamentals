c16376d299cb5129d7f3234ae64c3c4ab9ce1bf2
rise289 <rise289@gmail.com>
2019-06-13 16:02:52 -0500

    Lesson 4 - My Application Overview

---
+++ src/com/fundamentals/java/House.java
@@ -0,0 +1,19 @@
+package com.fundamentals.java;
+/*
+This is our first class that we created ourselves.
+*/
+public class House {
+
+    String rooftype;
+    int windowsize;
+    String foundation;
+    String doorcolor;
+
+    //Our first method (function)
+    public void doorOpenClose(){
+        System.out.println("Open my door");
+    }
+
+
+
+}
