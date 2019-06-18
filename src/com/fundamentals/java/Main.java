718a4cdb81b80a5b71ba008647470fd65c14f209
rise289 <rise289@gmail.com>
2019-06-18 10:42:19 -0500

    Lesson 5

--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -2,12 +2,28 @@ package com.fundamentals.java;
 
 /*
 This class is the starting class of this project.
-This is an example of a multi-line comment.
- */
+        This is an example of a multi-line comment.
+        */
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Lesson5Examples();
+        //houseExample();
+
+    }
+
+    public static void Lesson5Examples(){
+        Lesson5 myLesson5 = new Lesson5();
+        myLesson5.showLocalExample();
+        myLesson5.showLocalWithParameter(10,12);
+        int example = myLesson5.showLocalWithReturn(15,8);
+        System.out.println(example);
+        myLesson5.showInstanceVariable();
+        myLesson5.showConversions();
+    }
+
+
+    public static void houseExamples() {
         System.out.println("Welcome to Java");
         House myHouse = new House();
         myHouse.doorOpenClose();
